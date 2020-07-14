2248982036db4d28e59083fd319917a2b562da3e
Jesus Medina <medinabotamail@gmail.com>
2020-07-14 15:06:26 -0400

    se agrego boton de contacto

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ $('document').ready(function(){
     $('.cargando').fadeOut('1000')
     console.log("DOM loaded");
 
+    $('.contacto').hide();
+    $('.menu-contacto').hide();
     //MENU
     $('.abrir-menu').on('click', function(){
         $('.site-header').hide();
@@ -20,6 +22,50 @@ $('document').ready(function(){
         $('.nuestra-gente').show();
         $('.nosotros').show();
         $('.site-footer').show();
+
         
     });
+
+    $('.btnContacto').on('click', function(){
+        $('.site-header').hide();
+        $('.trabajos-clientes').hide();
+        $('.nuestra-gente').hide();
+        $('.nosotros').hide();
+        $('.site-footer').show();
+
+        $('.menu-contacto').show();
+        $('.menu-mobile').fadeOut(300);
+        $('.contacto').show();
+    })
+
+    $('.menu-contacto').on('click', function(){
+        $('.contacto').hide();
+        $('.site-footer').hide();
+        $('.contacto-mobile').fadeIn(300);
+    })
+
+    $('.contacto-mobile img').on('click', function(){
+        // $('.site-header').hide();
+        // $('.trabajos-clientes').hide();
+        // $('.nuestra-gente').hide();
+        // $('.nosotros').hide();
+        // $('.abrir-menu').hide();
+        $('.site-footer').show();
+        $('.contacto').show();
+        $('.contacto-mobile').fadeOut(300);
+    })    
+
+    $('.ir-inicio').on('click', function(){
+        $('.contacto-mobile').fadeOut(300);
+        $('.site-footer').show();
+        $('.contacto').hide();
+        $('.menu-contacto').hide();
+
+                $('.site-header').show();
+        $('.trabajos-clientes').show();
+        $('.nuestra-gente').show();
+        $('.nosotros').show();
+        $('.abrir-menu').show();
+
+    })
 });
