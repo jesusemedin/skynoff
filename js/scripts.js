b757fa8761342f12544dbe693491a95475f7a6d5
Jesus Medina <medinabotamail@gmail.com>
2020-07-13 21:14:16 -0400

    se cargo poster

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ $('document').ready(function(){
 
     //MENU
     $('.abrir-menu').on('click', function(){
-        $('.abrir-menu').hide();
+        $('.site-header').hide();
         $('.menu-mobile').fadeIn(300);
 
       });
     $('.menu-mobile img').on('click', function(){
         $('.menu-mobile').fadeOut(300);
-        $('.abrir-menu').show();
+        $('.site-header').show();
     });
 });
